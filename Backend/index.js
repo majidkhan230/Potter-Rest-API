0056d4d08285b6fbed6f4cac1cdb44aba4e1634d
MajidKh230 <khanmajid230@gmail.com>
2024-12-03 21:53:09 +0500

    fix routes

--- Backend/index.js
+++ Backend/index.js
@@ -2,31 +2,33 @@ const express = require("express");
 const bookRoutes = require("./routes/bookRoutes");
 const characterRoutes = require("./routes/characterRoutes");
 const housesRoutes = require("./routes/housesRoutes");
-const spellRoutes = require("./routes/spellRoutes");
-const dotenv =require('dotenv').config()
-
-
+const spellRoutes = require("./routes/spellRoutes.js");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
+app.use(express.json());
 
 
-app.use(express.json())
 
 
-app.get("/en",(req,res)=>{
-    res.status(200).send({message:"welcome to backend"})
-})
 
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message:
+      "This is PotterAPI, a REST API that stores images and information about Harry Potter characters, books and spells. For more info about the routes and query params, visit the github repo.",
+    repo: "https://github.com/majidkhan230/Potter-Rest-API/",
+    routes: ["/books", "/characters", "/houses", "/spells"],
+  });
+});
 
-app.use("/en/books",bookRoutes)
-app.use("/en/characters",characterRoutes)
-app.use("/en/houses",housesRoutes)
-app.use("/en/spells",spellRoutes)
+app.use("/books", bookRoutes);
+app.use("/characters", characterRoutes);
+app.use("/houses", housesRoutes);
+app.use("/spells", spellRoutes);
 
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT,()=>{
-    console.log(`server is sucessfully started at ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is sucessfully started at ${PORT}`);
+});
